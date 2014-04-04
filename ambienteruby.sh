5416b9c48f6a700cc58e114df00a2ac71b77dcbc
Jorge <jorge@emiolo.com>
2014-04-04 15:54:42 -0300

    Primeira versao do script para criar ambiente ruby no vagrant

---
+++ ambienteruby.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+echo "Execução automática de scripts,apenas aguarde..."
+#atualiza o ambiente...
+echo "Atualizando ambiente..."
+sudo apt-get update
+clear
+
+#instalando curl para baixar o rvm...
+echo "Instalando o curl..."
+sudo apt-get install curl
+clear
+
+#instalando o rvm...
+echo "Instalando rvm..."
+\curl -L https://get.rvm.io | bash -s stable &&
+clear
+
+#inicializando rvm...
+echo "inicializando rvm..."
+source ~/.rvm/scripts/rvm
+clear
+
+#Verificando e instalando as dependências da rvm...
+echo "Verificando e instalando as dependências da rvm"
+rvm requirements
+clear
+
+#Instalando ruby...
+echo "Instalando Ruby"
+rvm install ruby
+clear
+
+#Verificando e instalando as dependências para instalar o rails...
+echo "Verificando e instalando as dependências para instalar o rails..."
+rvm rubygems current 
+clear
+
+#Instalando framework Ruby on Rails
+echo "Instalando framework Ruby on Rails"
+gem install rails
+clear
